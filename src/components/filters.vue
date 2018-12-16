d8983e715e7cf119fab177ca87bd48e14e1a9062
Andy <andy@192.168.1.7>
2018-12-16 21:47:17 +0200

    Vue setup

---
+++ src/components/filters.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="filters absolute-horizontal-center" v-bind:class="{'filters-displayed': shouldDisplay }">
+		<button @click="toggleFilters">Filters</button>
+		
+
+		<button @click="toggleFilters">Set Filters</button>
+
+	</div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+	name: 'Filters',
+	props: ['display'],
+	data: function() {
+		console.log('pro', this.display)
+		return {
+			shouldDisplay: this.display
+		}
+	},
+	methods: {
+		toggleFilters: function() {
+			this.displayFilters = !this.displayFilters;
+			console.log("this.displayFilters", this.displayFilters);
+		}
+	}
+}
+</script>
+
+<style scoped>
+.filters {
+	position: absolute;
+	top: 0px;
+
+	opacity: 0;
+	width: 300px;
+	background-color: tomato;
+	transition: all .3s;
+}
+
+.filters.filters-displayed {
+	opacity: 1;
+	top: 60px;
+}
+
+button {
+	display: block;
+	margin: auto;
+}
+</style>
---
+++ src/components/search.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="search">
+		<input type="text" v-model="message" class="search-input"/>
+		<Filters />
+		<p>{{message}}</p>
+	</div>
+</template>
+
+<script>
+/* eslint-disable */
+import Filters from './filters.vue';
+
+export default {
+	name: 'Search',
+	components: {
+		Filters
+	},	
+	data: function() {
+		return {
+			message: ''
+		}
+	}
+}
+</script>
+
+<style scoped>
+.search {
+	position: relative;
+}
+.search-input {
+	width: 500px;
+	padding: 10px;
+	margin: auto;
+	border-radius: 5px;
+	border: 1px solid #888;
+}
+</style>
